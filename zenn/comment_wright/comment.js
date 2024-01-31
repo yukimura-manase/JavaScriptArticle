18c4554d9394c68c32eda83b84fd82e4dcf44592
yukimura-manase <masahiro19951212@gmail.com>
2024-01-31 21:02:13 +0900

    コメントについての記事ネタ作成

--- zenn/comment_wright/comment.js
+++ zenn/comment_wright/comment.js
@@ -1,3 +1,7 @@
+// [ プロフェッショナル 〜コメントの流儀〜 ]
+
+// Type1. 関数に対するコメント
+
 /**
  * NOTE: 2つの数値の間の乱数を返す関数
  * @name makeRundom - 乱数生成
@@ -7,4 +11,52 @@
  */
 function makeRundom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Type2. クラスに対するコメント
+
+/**
+ * NOTE: クラスの説明
+ * @class
+ */
+class ClassName {
+  /**
+   * NOTE: コンストラクタの説明
+   * @constructor
+   * @param {string} name - 名前
+   */
+  constructor(name) {
+    this.name = name;
+  }
+
+  /**
+   * NOTE: メソッドの説明
+   * @method
+   * @param {string} name - 名前
+   */
+  method(name) {
+    this.name = name;
+  }
+}
+
+// Type3. 変数に対するコメント
+
+/**
+ * NOTE: 変数の説明
+ * @type {string}
+ */
+const variable = "value";
+
+// Type4. プロパティに対するコメント
+
+/**
+ * NOTE: クラスの説明
+ * @class
+ */
+class ClassName {
+  /**
+   * NOTE: プロパティの説明
+   * @type {string}
+   */
+  prop = "value";
 }
